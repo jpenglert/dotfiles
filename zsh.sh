7cd1d5d6a8cca3f56f083b2b87cb7912563233fd
Jesse Englert <jenglert@gmail.com>
2021-03-06 08:52:55 -0800

    Add powerlevel9k theme

--- zsh.sh
+++ zsh.sh
@@ -2,3 +2,6 @@
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
