07caaeaa415d150ace571137e69252cc5aa4335c
Jesse Englert <jenglert@gmail.com>
2021-03-08 08:58:30 -0800

    Add powerline fonts

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 
-# Install Oh-My-Zsh
+# Install Oh-My-Zsh ###########################################################
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install powerlevel9k theme
+# Install powerlevel9k theme ##################################################
+# See https://github.com/Powerlevel9k/powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install powerline fonts #####################################################
+# See https://github.com/powerline/fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
