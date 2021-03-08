60afd0cc2b2e68f94cc4e513fbc48267f70b1aec
Jesse Englert <jenglert@gmail.com>
2021-03-08 08:38:08 -0800

    Update bootstrap.sh for zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 cd "$(dirname "${BASH_SOURCE}")";
 
@@ -13,15 +13,16 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "This may overwrite existing files in your home directory. Are you sure? (y/n) ";
+    read answer
+
+	if [[ $answer =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
 fi;
