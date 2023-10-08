268d5c6cc3ae34e38309c179d1ebc86d68f8bafe
Jesse Englert <jenglert@gmail.com>
2023-10-08 13:03:29 -0700

    feat: clean up brew.sh

--- brew.sh
+++ brew.sh
@@ -22,15 +22,6 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed
-# Install a modern version of Bash.
-# brew install bash
-# brew install bash-completion@2
-
-# Switch to using brew-installed bash as default shell
-# if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-#   chsh -s "${BREW_PREFIX}/bin/bash";
-# fi;
 
 # Install `wget`
 brew install wget
@@ -41,41 +32,8 @@ brew install gnupg
 # Install more recent versions of some macOS tools.
 brew install vim
 brew install grep
-# brew install openssh
 brew install screen
 brew install gmp
-
-# Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
 
 # Install other useful binaries.
 brew install ack
